1e9c6cc2ad7f457a164579bdb03f9c29e56f7195
ekkim <provenceblue1498@gmail.com>
2022-01-25 14:53:15 +0900

    store 구조 적용

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,28 +1,24 @@
 <template>
     <div>
-        <div v-for="user in users" v-bind:key="user.title">{{user.title}}</div>
+        <div v-for="user in this.$store.state.news" v-bind:key="user.title">{{user.title}}</div>
     </div>
 </template>
 
 <script>
 
-import {fetchNewsList} from '../api/index.js';
+
 export default {
-    data() {
-        return{
-            users: []
-        }
-    },
     created(){
-        var vm = this;
-        fetchNewsList()
-            .then(function(response){
-                console.log(response);
-                vm.users = response.data;
-            })
-            .catch(function(error){
-                console.log(error);
-            })
+        this.$store.dispatch('FETCH_NEWS');
+        // var vm = this;
+        // fetchNewsList()
+        //     .then(function(response){
+        //         console.log(response);
+        //         vm.users = response.data;
+        //     })
+        //     .catch(function(error){
+        //         console.log(error);
+        //     })
     }
 };
 </script>
