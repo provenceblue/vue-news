a770c2c437b1f1f810bf9a8003e50e312990f9ba
ekkim <provenceblue1498@gmail.com>
2022-01-28 10:02:36 +0900

    사용자컴포넌트 데이터 흐름처리 1

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="user-container">
+        <div>
+            <i class="fas fa-user"></i>
+        </div>
+        <div class="user-description">
+            <!-- <router-link :to="`/user/${userInfo.user}`">{{userInfo.user}}</router-link>
+            <div class="time">{{userInfo.time_ago}}</div> -->
+            <p>name : {{userInfo.id}}</p>
+            <p>karma : {{userInfo.karma}}</p>
+            <p>created : {{userInfo.created}}</p>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        userInfo(){
+            return this.$store.state.user;
+        }
+    },
+    
+};
+</script>
+
+<style scoped>
+.user-container{display:flex;align-items:center;padding:0.5rem;}
+.fa-user{font-size:2.5rem;}
+.user-description{padding-left:8px;}
+.time{font-size:0.7rem;}
+</style>
--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,26 +1,26 @@
 <template>
     <div>
-        <p>name : {{userInfo.id}}</p>
+        <user-profile></user-profile>
+        <!-- <p>name : {{userInfo.id}}</p>
         <p>karma : {{userInfo.karma}}</p>
-        <p>created : {{userInfo.created}}</p>
+        <p>created : {{userInfo.created}}</p> -->
     </div>
 </template>
 
 <script>
 
+import UserProfile from '../components/UserProfile.vue';
 export default {
-    computed: {
-        userInfo(){
-            return this.$store.state.user;
-        }
+    components: {
+        UserProfile,
     },
+    
     created(){
         
         const userName = this.$route.params.id
         //console.log(userName)
         this.$store.dispatch('FETCH_USER',userName);
     },
-    
 };
 </script>
 
