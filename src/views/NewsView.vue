b1ce1964531e1962a22585440cadf08a1f5e3b78
ekkim <provenceblue1498@gmail.com>
2022-02-04 14:58:55 +0900

    mixin 실습

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -6,26 +6,13 @@
 
 <script>
 import ListItem from '../components/ListItem.vue';
-import bus from '../utils/bus.js';
+import ListMixin from '../mixins/ListMixin.js';
+
 export default { 
     components:{
         ListItem,
     },
-    created(){
-        bus.$emit('start:spinner');
-        setTimeout(() => {
-            this.$store.dispatch('FETCH_NEWS')
-            .then(() => {
-                console.log('fetched');
-                bus.$emit('end:spinner');
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-        },3000)
-        
-
-        //bus.$emit('end:spinner');
-    },
+    mixins: [ListMixin],
+    
 };
 </script>
